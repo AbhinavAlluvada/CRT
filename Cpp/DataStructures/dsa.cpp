75d6272222293f35116405a085da96c6bb7af0f7
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2026-03-02 10:32:36 +0530

    Format commit

---
+++ Cpp/DataStructures/dsa.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "DSA Intro"<<endl;
+    vector <int> vect = {1,2,3,4,5,6};
+    int n = vect.size();
+    for(int i = n-1;i>=0;i--){
+        cout << vect[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
